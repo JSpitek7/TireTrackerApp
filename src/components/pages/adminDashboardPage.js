6022856370b061b6b94d82df6e8eb16cec9022f8
JSpitek7 <jspitek7@vt.edu>
2019-04-01 14:50:37 -0400

    created app framework

---
+++ src/components/pages/adminDashboardPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class AdminDashboardPage extends Component {
+    render() {
+        return(
+            <div>
+                The admin home page will go here.
+            </div>
+        )
+    }
+}
+
+export default AdminDashboardPage;
---
+++ src/components/pages/tireReplacementPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class TireReplacementPage extends Component {
+    render() {
+        return(
+            <div>
+                The tire replacement page will go here.
+            </div>
+        )
+    }
+}
+
+export default TireReplacementPage;
